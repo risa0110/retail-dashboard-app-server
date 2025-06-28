6d0399d8c0694d130a37cfd5e28d923f946d7edd
Risa Hasegawa <r0121m1014@gmail.com>
2025-06-28 15:40:17 -0700

    add server files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path =require('path');
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/register', (req,res)=>{
+    const userData = req.body;
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    let users = [];
+
+    if(fs.existsSync(filePath)){
+        const fileData = fs.readFileSync(filePath);
+        users = JSON.parse(fileData);
+    }
+    users.push(userData);
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+    
+    res.status(200).json({message:'User registered successfully!'});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+})
